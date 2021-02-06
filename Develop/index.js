0baaaf0e92923628f471a1bd1efcc93d542fa60f
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-02-06 20:33:42 +1100

    adding to TO DOs, moving questions into array

--- Develop/index.js
+++ Develop/index.js
@@ -4,26 +4,48 @@ const inquirer = require('inquirer');
 
 //from Homework activity example
 inquirer
-.prompt([
-    
-    {type: "input",
+.prompt(questions[
+    {
+    type: "input",
+    message: "what is your project?",
     name: "title",
-    message: "what is your project?"
     }
-
-]
-
-)
+]) 
+console.log(questions);
 // TODO: Create an array of questions for user input
 
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "what is your project?",
+        name: "title",
+    },
+
+
+];
+//create the array then call on it instead of listing all the questions in .prompt/ test if both work. 
 //title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
+// here the other functions including questions.response will be input 
 function init() {}
 
+//
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+  
+  asyncCall(); 
+
+  //async call is function init();
+  
+
 // Function call to initialize app
+//this goes last; makes everything on the page run
 init();
