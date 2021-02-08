3382ed835141675ef369a149d87967c010aca070
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-02-08 13:44:32 +1100

    filled in messages for questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,49 +7,50 @@ const markdown = require('./utils/generateMarkdown.js');
 
 console.log(questions);
 // TODO: Create an array of questions for user input
+// need to add username, email and link to github
 
 const questions = [
     {
         type: "input",
-        message: "what is your project?",
+        message: "What is your project?",
         name: "title",
     },
     {   type: "input",
-        message: "provide a desription for your project",
+        message: "Please provide a desription for your project",
         name: "description",
         
     },
     {   type: "input",
-        message: "",
+        message: "Please provide a list for table of contents",
         name: "table of contents ",
 
     },
     {   type: "input",
-        message: "",
+        message: "provide information on installation",
         name: "installation",
 
     },
     {
         type: "input",
-        message: "",
+        message: "provide information for usage",
         name: "usage",
 
     },
     {
         type: "input",
-        message: "",
+        message: "Please provide the licenses to badges",
         name: "license",
 
     },
     {
         type: "input",
-        message: "",
+        message: "Please provide information on any contributors",
         name: "contributing",
 
     },
     {
         type: "input",
-        message: "",
+        message: "Please provide the required tests",
         name: "tests",
 
     },
@@ -93,9 +94,7 @@ async function asyncCall() {
   }
   
   asyncCall(); 
-
   //async call is function init();
-  
 
 // Function call to initialize app
 //this goes last; makes everything on the page run
